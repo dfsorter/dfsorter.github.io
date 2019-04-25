7d8ff24b5f515e4e32b6c90eae0b1ea7fd14f4c3
Cynthia Zhong <cynthia.zz@berkeley.edu>
2019-04-24 18:13:50 -0700

    added about page and funcationality for tab buttons

--- dfs-script.js
+++ dfs-script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	
+	$(".header-tab").click(function() {
+		$(".tab-page").hide();
+		var id = $(this).attr("id");
+		var page_id = "#" + id + "-page";
+		$(page_id).fadeIn();
+	})
 })
